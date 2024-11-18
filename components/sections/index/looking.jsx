30030fbabcae1cd0ad4ba8ec77b17ad203d78e83
themainax <alvor.mi.na@gmail.com>
2024-11-18 17:47:12 +0300

    google verification

--- components/sections/index/looking.jsx
+++ components/sections/index/looking.jsx
@@ -23,13 +23,18 @@ export default function Looking() {
 	return (
 		<Section classProp={`${looking.section} borderBottom`}>	
 			<Container classProp={`${section.title} ${looking.container}`} spacing={['verticalXXXLrg']}>
-				<h1>&apos;Hire me</h1>
+				<h1> What can i do? </h1>
 				<h2 className={looking.json}>: &#123;</h2>
-				<h2 className={looking.jsonSub}><span className={looking.highlight}>Web Developer</span></h2>
-				<h2 className={looking.jsonSub}><span className={looking.highlight2}>Mobile App Developer</span></h2>
-				<h2 className={looking.jsonSub}><span className={looking.highlight2}>Technician</span></h2>
-				<h2 className={looking.jsonSub}><span className={looking.highlight}>Server Admin</span></h2>
-				<h2 className={looking.jsonSub}><span className={looking.highlight}>Cybersecurity Expert</span></h2>
+				<h2 className={looking.jsonSub}><span className={looking.highlight}> I create websites / Webapps </span></h2>
+				<br />
+				<h2 className={looking.jsonSub}><span className={looking.highlight2}> I create mobile apps ios & android</span></h2>
+				<br />
+				<h2 className={looking.jsonSub}><span className={looking.highlight2}> I solve computer, Mobile & server issues</span></h2>
+				<br />
+				<h2 className={looking.jsonSub}><span className={looking.highlight}> I cofigure web servers, Mail Servers </span></h2>
+				<br />
+				<h2 className={looking.jsonSub}><span className={looking.highlight}> I also offer Cybersecurity  consultation</span></h2>
+				<br />
 				<h2 className={looking.json}>&#125;</h2>
 				<h4>Let’s turn your ideas into reality with my expertise.</h4>
 				<p className="subtitle">with a focus on Product Design.</p>
